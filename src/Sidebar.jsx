cb44b8ad43030c4b837482753b6af1f850e867de
Mordy fandio <ordybeni@gmail.com>
2023-08-30 23:55:48 +0100

    update the sidebarMusic

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,12 +1,16 @@
-import {FaUser,FaBars,FaTimes, FaAngleRight,FaGlobe, FaAngleDown} from 'react-icons/fa';
+import {FaUser,FaBars,FaTimes, FaAngleRight,FaGlobe, FaAngleDown,FaArrowLeft} from 'react-icons/fa';
 import { useState,useEffect,useRef } from 'react';
 import { Link } from "react-router-dom";
 import "./style.css";
 
+
 const Sidebar = () => {
     const [SideBar,setSideBar]=useState(false);
     const showSideBar= () => setSideBar(!SideBar);
-    const msgEndRef=useRef(null);
+    
+    const [SideBarMusic,setSideBarMusic]=useState();
+    const showSideBarMusic= () => setSideBarMusic(!SideBarMusic);
+    
 
     let menuRef = useRef();
    
@@ -23,9 +27,6 @@ const Sidebar = () => {
       }
     });
     
-    useEffect(()=>{
- 
-    });
 
     return (  
         <>
@@ -41,31 +42,47 @@ const Sidebar = () => {
             <div className ="Sign">
                 <p><FaUser style={{color:"white",cursor:"pointer",fontSize:"1.5em"}} /> Hello, Sign In </p>
             </div>
-            <div className='subinfo'>
+            <div className='subinfo' >
                 <article>
                     <h2>Digital Content & Devices</h2>
                     <ul className='menuArticle'>
-                        <li>Music <FaAngleRight className='angleRight'/></li>
-                        <li>AppStore <FaAngleRight className='angleRight'/></li>
-                        <li>Books <FaAngleRight className='angleRight'/></li>
+                        <li onClick={showSideBarMusic} style={{position:'relative'}}><Link >Music <FaAngleRight className='angleRight' onClick={showSideBarMusic}/>
+                        <div className={SideBarMusic ? 'openMusic active':'Music'} style={{position:'absolute'}}>
+                            <div className="scrollbar">
+                                <FaArrowLeft className='btnArrow'/>
+                                <span>Main Menu</span>
+                            </div>
+                            <div className='contains'>
+                                <h1>Stream Music</h1>
+                                <ul>
+                                    <li>Music Unlimited</li>
+                                    <li>Free Music</li>
+                                    <li>Open Web player</li>
+                                    <li>Download the app</li>
+                                </ul>
+                            </div>
+                        </div>
+                        </Link></li>
+                        <li className='menuChange'><Link>AppStore <FaAngleRight className='angleRight'/></Link></li>
+                        <li className='menuChange'><Link>Books <FaAngleRight className='angleRight'/></Link></li>
                     </ul>
                 </article>
                 <article>
                     <h2>Shop By Department</h2>
                     <ul className='menuArticle'>
-                        <li>Computers <FaAngleRight className='angleRight'/></li>
-                        <li>Electronics <FaAngleRight className='angleRight'/></li>
-                        <li>Smart Phone <FaAngleRight className='angleRight'/></li>
-                        <li>See All <FaAngleDown className='angleDown'/></li>
+                        <li className='menuChange'><Link>Computers <FaAngleRight className='angleRight'/></Link></li>
+                        <li className='menuChange'><Link>Electronics <FaAngleRight className='angleRight'/></Link></li>
+                        <li className='menuChange'><Link>Smart Phone <FaAngleRight className='angleRight'/></Link></li>
+                        <li className='menuChange'><Link>See All <FaAngleDown className='angleDown'/></Link></li>
                     </ul>
                 </article>
                 <article>
                     <h2>Programs & Features</h2>
                     <ul className='menuArticle'>
-                      <li>Gift Cards <FaAngleRight className='angleRight'/></li>
-                      <li>Shop by interest <FaAngleRight className='angleRight'/></li>
-                      <li>International Shopping <FaAngleRight className='angleRight'/></li>
-                      <li>See All <FaAngleDown className='angleDown'/></li>
+                      <li className='menuChange'><Link>Gift Cards <FaAngleRight className='angleRight'/></Link></li>
+                      <li className='menuChange'><Link>Shop by interest <FaAngleRight className='angleRight'/></Link></li>
+                      <li className='menuChange'><Link>International Shopping <FaAngleRight className='angleRight'/></Link></li>
+                      <li className='menuChange'><Link>See All <FaAngleDown className='angleDown'/></Link></li>
                     </ul>
                 </article>
                 <article>
@@ -79,8 +96,9 @@ const Sidebar = () => {
                 </article>
             </div>
         </div>
-        <div ref={msgEndRef}></div>
         </>
     );
 }
+
+
 export default Sidebar;
